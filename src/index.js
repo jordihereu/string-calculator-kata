294cb03366536fd49e8f4c4551a6340617a24bb3
Jordi Hereu <iogurt@gmail.com>
2021-02-10 16:41:35 +0100

    mob programming: Pau, Juan Pablo, Anna and Roberto

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-export class StringCalculator {
+export function Add(calculatorInput) {
+  let delimiter = ',';
+  let normalizedString;
+
+  if (!calculatorInput) {
+    return 0
+  }
+
+  if (hasOptionalDelimiter(calculatorInput)) {
+    delimiter = getOptionalDelimiter(calculatorInput);
+    normalizedString = calculatorInput.split('\n')[1];
+  } else {
+    normalizedString = calculatorInput.replace('\n', delimiter)
+  }
+  return sumNumbers(normalizedString, delimiter);
+}
+
+function hasOptionalDelimiter(input) {
+  return input.startsWith('//')
+}
+
+function getOptionalDelimiter(calculatorInput) {
+  let lines = calculatorInput.split('\n')
+  return lines[0].replace('//', '')
+}
+
+function sumNumbers(numbersString, delimiter) {
+  const array = numbersString.split(delimiter)
+  return array.reduce((total, num) => +num + total, 0)
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,46 @@
-import { StringCalculator } from '../src/index.js'
+import { Add } from '../src/index.js'
 
-describe('Write your first test:', () => {
-  test('it should be useful, easy and fun!', () => {
-    expect(1).toBe(0)
-  })
+describe('String Calculator Kata Tests', () => {
+  it('should return zero when the parameter is an empty string', () => {
+    expect(Add('')).toBe(0);
+  });
+
+  it('should return the input number when the string is only one number', () => {
+    expect(Add('2')).toBe(2);
+  });
+
+  it('should return the input number when string is one number of several digits', () => {
+    const string = '100';
+    
+    const result = Add(string);
+
+    expect(result).toBe(100);
+  });
+
+  it('should return sum', () => {
+    const string = '1,2,3,4,5';
+
+    const result = Add(string);
+    
+    expect(result).toBe(15);
+  });
+
+  it('should return sum when having new lines', () => {
+    const string = '1,2,3,4\n5';
+
+    const result = Add(string);
+    
+    expect(result).toBe(15);
+  });
+
+  it('should allow custom separators', () => {
+    const separator = '//;\n';
+    const string = separator + '1;2';
+
+    const result = Add(string);
+    
+    expect(result).toBe(3);
+  });
+
+  
 })
