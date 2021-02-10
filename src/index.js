3764071ee1b791f256946dc0e99d41fc9cb4f399
Jordi Hereu <iogurt@gmail.com>
2021-02-10 14:09:05 +0100

    adding a dummy comment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-export class StringCalculator{   
-
-    Add(numbers) {
-        if(!numbers){            
-            return 0
-        }
+export class StringCalculator {
+  //this is the string calculator kata main method
+  Add(numbers) {
+    if (!numbers) {
+      return 0
     }
+  }
 }
